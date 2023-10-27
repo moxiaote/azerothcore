bb1b6f48ccac56818dfbec6f0266c3332beec667
bgcfb <bgcfb@qq.com>
2023-10-28 00:24:05 +0800

    降低HTOC箱子需求,允许团灭5次

--- src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/instance_trial_of_the_crusader.cpp
+++ src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/instance_trial_of_the_crusader.cpp
@@ -1442,9 +1442,9 @@ public:
                             uint32 tributeChest = 0;
                             if( instance->GetSpawnMode() == RAID_DIFFICULTY_10MAN_HEROIC )
                             {
-                                if (AttemptsLeft >= 50)
+                                if (AttemptsLeft >= 45)//为照顾玩家,降低50箱子难度
                                     tributeChest = GO_TRIBUTE_CHEST_10H_99;
-                                else if (AttemptsLeft >= 45)
+                                else if (AttemptsLeft >= 40)//为照顾玩家,降低45箱子难度
                                     tributeChest = GO_TRIBUTE_CHEST_10H_50;
                                 else if (AttemptsLeft >= 25)
                                     tributeChest = GO_TRIBUTE_CHEST_10H_45;
@@ -1453,9 +1453,9 @@ public:
                             }
                             else if( instance->GetSpawnMode() == RAID_DIFFICULTY_25MAN_HEROIC )
                             {
-                                if (AttemptsLeft >= 50)
+                                if (AttemptsLeft >= 45)//为照顾玩家,降低50箱子难度
                                     tributeChest = GO_TRIBUTE_CHEST_25H_99;
-                                else if (AttemptsLeft >= 45)
+                                else if (AttemptsLeft >= 40)//为照顾玩家,降低45箱子难度
                                     tributeChest = GO_TRIBUTE_CHEST_25H_50;
                                 else if (AttemptsLeft >= 25)
                                     tributeChest = GO_TRIBUTE_CHEST_25H_45;
